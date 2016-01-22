6f0198865c10832f20cdecbaa37067ce5a3c6c23
Vasily <vasily-nosikov@yandex.ru>
2016-01-22 13:26:45 -0300

    copying templates to a canvas feature

--- elements-list.js
+++ elements-list.js
@@ -9,20 +9,17 @@ $(document).ready(function(){
 
 	$('.template').draggable({
 		helper: "clone",
-		drag: checkIfIsInCanvas,
+		drag: colorizeBox,
 		stop: copyTemplateToCanvas 
 	});
 
-	function checkIfIsInCanvas(event, ui){		
-		var x = ui.offset.left;
-		var y = ui.offset.top;
-
-		console.log("(" + x + ", " + y + ")");
-
-		if(x >= canvasMinX && x <= canvasMaxX && y >= canvasMinY && y <= canvasMaxY ){
+	function colorizeBox(event, ui){		
+		//If we are inside the canvas let's paint our element green
+		if(checkIfIsInCanvas(ui.offset)){
 			ui.helper.removeClass('redmask');
 			ui.helper.addClass('greenmask');
 		}
+		//Otherwise it will be red
 		else{
 			ui.helper.removeClass('greenmask');
 			ui.helper.addClass('redmask');	
@@ -30,9 +27,32 @@ $(document).ready(function(){
 	}
 
 	function copyTemplateToCanvas(event, ui){
-		
-		if(true){
+		if(checkIfIsInCanvas(ui.offset)){
+			var newCanvasElement = $("<div class='box element'>");
+			var elementText = $("<div class='text'>ABCDE</div>");
+			var html = ui.helper.find('.text').html();
+
+			elementText.html(html);
+
+			newCanvasElement.append(elementText);
+			$('.canvas').append(newCanvasElement);
+
+			newCanvasElement.offset(ui.offset);
 
 		}
+
+		else console.log("Impossible to copy template outside of a canvas");
+	}
+
+	function checkIfIsInCanvas(offset){
+		var x = offset.left;
+		var y = offset.top;
+
+		console.log("(" + x + ", " + y + ")");
+
+		if(x >= canvasMinX && x <= canvasMaxX && y >= canvasMinY && y <= canvasMaxY ){
+			return true;
+		}
+		else return false;
 	}
 });
